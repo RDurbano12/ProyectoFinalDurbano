38f02e4271a3b4c40f22d9c85f120d5cd28db9d6
RDurbano <rdurbano@outlook.com>
2023-01-26 12:19:01 -0300

    modo oscuro

--- js/mostrador.js
+++ js/mostrador.js
@@ -53,3 +53,28 @@ if (productoEnCarrito){
     localStorage.setItem("carrito",JSON.stringify(carrito));
     }
 }
+
+
+
+//modo oscuro:
+const bdark= document.querySelector('#bdark');
+const body =document.querySelector('body');
+
+load()
+
+bdark.addEventListener('click', e =>{
+    body.classList.toggle('darkmode');
+    store(body.classList.contains('darkmode'));
+});
+
+function load(){
+    const darkmode =localStorage.getItem('darkmode');
+    if(!darkmode){
+        store('false');
+    }else if(darkmode =='true'){
+        body.classList.add('darkmode');
+    }
+}
+function store (value){
+    localStorage.setItem('darkmode',value);
+}
